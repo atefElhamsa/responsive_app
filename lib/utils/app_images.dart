c5add902cb493f6ab023d4427df49e5c9dacba51
atefElhamsa <atefelhamsa79@gmail.com>
2025-05-02 15:34:48 +0300

    add images , fonts , textStyles

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,30 @@
+class Assets {
+  Assets._();
+
+  static const String imagesAvatar1 = "assets/images/avatar_1.svg";
+
+  static const String imagesAvatar2 = "assets/images/avatar_2.svg";
+
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  static const String imagesCardBackground =
+      "assets/images/card_background.svg";
+
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  static const String imagesIncome = "assets/images/income.svg";
+
+  static const String imagesInvestments = "assets/images/investments.svg";
+
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  static const String imagesMytransaction = "assets/images/mytransaction.svg";
+
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  static const String imagesStatics = "assets/images/statics.svg";
+
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
