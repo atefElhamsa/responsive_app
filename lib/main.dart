f00ed21a3e8b63adfa1e86d251ddec452696c28a
atefElhamsa <atefelhamsa79@gmail.com>
2025-05-02 14:04:37 +0300

    add adaptive layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_app_dash_board/views/dash_board_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ResponsiveDashBoard());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ResponsiveDashBoard extends StatelessWidget {
+  const ResponsiveDashBoard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: DashBoardView(),
     );
   }
 }
