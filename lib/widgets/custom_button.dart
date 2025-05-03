3de52b9fb1873f9ea25fa9e984bf6bda5b0ab91f
atefElhamsa <atefelhamsa79@gmail.com>
2025-05-03 22:44:53 +0300

    quick invoice done

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app_dash_board/utils/app_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, this.backgroundColor, this.textColor, required this.title});
+  final Color? backgroundColor, textColor;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 62,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: backgroundColor ?? const Color(0XFF4DB7F2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          title,
+          style: AppStyles.styleSemiBold18(context).copyWith(color: textColor),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/dash_board_desktop_layout.dart
+++ lib/widgets/dash_board_desktop_layout.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_app_dash_board/widgets/all_expensess.dart';
 import 'package:responsive_app_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_app_dash_board/widgets/quick_invoice.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const DashBoardDesktopLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
-        Expanded(child: CustomDrawer()),
-        SizedBox(width: 32),
+        const Expanded(child: CustomDrawer()),
+        const SizedBox(width: 32),
         Expanded(
           flex: 2,
-          child: Column(children: [Expanded(child: AllExpensess())]),
+          child: ListView(
+            children: const [
+              SizedBox(height: 40),
+              AllExpensess(),
+              SizedBox(height: 24),
+              QuickInvoice(),
+            ],
+          ),
         ),
       ],
     );
