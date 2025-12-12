e37256107d5ffa784a68c6d45987676849148186
atefElhamsa <atefelhamsa79@gmail.com>
2025-12-12 14:40:25 +0200

    edit

--- lib/widgets/my_cards_and_transction_history_section.dart
+++ lib/widgets/my_cards_and_transction_history_section.dart
@@ -9,15 +9,16 @@ class MyCardsAndTransctionHistorySection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CustomBackgroundContainer(
-        child: Column(
-      children: [
-        MyCardsSection(),
-        Divider(
-          height: 40,
-          color: Color(0xffF1F1F1),
-        ),
-        TrasnctionHistory(),
-      ],
-    ));
+      child: Column(
+        children: [
+          MyCardsSection(),
+          Divider(
+            height: 40,
+            color: Color(0xffF1F1F1),
+          ),
+          TrasnctionHistory(),
+        ],
+      ),
+    );
   }
 }
